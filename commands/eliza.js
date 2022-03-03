3d9983ce74e2e8fccd25218876827c6e08559693
ClaimedLizard <leolin93@live.com>
2022-03-03 13:35:52 -0800

    Eliza command basic functionality finished

--- commands/eliza.js
+++ commands/eliza.js
@@ -3,6 +3,9 @@ const { client, isLegal } = require('../client.js');
 const Eliza = require('../eliza-as-promised');
 const eliza = new Eliza();
 const goodWords = ['I agree', 'You\'re the best', 'Wise words', 'You\'re so smart', 'Nice', 'Absolutely', 'Definitely', 'Exactly', 'You\'re right', 'I couldn\'t agree more', 'That\'s true', 'That\'s for sure'];
+// If the doctor is in, then reply with eliza responses
+// If false, then reply with agreement messages
+let theDoctorIsIn = false;
 
 // Robocop
 client.on('messageCreate', (message) => {
@@ -10,8 +13,22 @@ client.on('messageCreate', (message) => {
     if (message.author.bot && message.author.id != message.guild.me.id) {
         message.react('🤖');
     }
+    // If the doctor is in, then reply with eliza
+    if (theDoctorIsIn && message.author.id != message.guild.me.id) {
+        const statement = message.content;
+        eliza.getResponse(statement).then(async (response) => {
+            if (response.reply) {
+                message.channel.send(response.reply);
+            }
+            else if (response.final) {
+                message.channel.send(response.final);
+                // Turn the doctor off
+                theDoctorIsIn = false;
+            }
+        });
+    }
     // Agree with wise words from wise men
-    if (isLegal(message.member)) {
+    else if (isLegal(message.member)) {
         const index = Math.floor(Math.random() * goodWords.length);
         // Send the message, and then delete it after 5 seconds
         message.channel.send(goodWords[index]).then((mes) => {
@@ -28,21 +45,17 @@ client.on('messageCreate', (message) => {
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('eliza')
-        .setDescription('Call the doctor. In Progress'),
+        .setDescription('Call the doctor'),
     async execute(interaction) {
-        const statement = 'I need some help';
-        eliza.getResponse(statement).then(async (response) => {
-            if (response.reply) {
-                await interaction.reply({ content:response.reply, ephemeral:true });
-                return;
-            }
-            if (response.final) {
-                await interaction.reply({ content:response.final, ephemeral:true });
-                return;
-            }
-        }).catch(async (err) => {
-            console.log(err);
-            await interaction.reply({ content:'Oops', ephemeral:true });
-        });
+        // Toggle the doctor on or off
+        theDoctorIsIn = !theDoctorIsIn;
+        if (theDoctorIsIn) {
+            await interaction.reply({ content:eliza.getInitial() });
+        }
+        else {
+            eliza.getResponse('Bye').then(async (response) => {
+                await interaction.reply({ content:response.final });
+            });
+        }
     },
 };
